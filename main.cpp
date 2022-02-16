95fd0a93b16709e72d7feba233002f18bdf7fcd9
Meher Lippmann <melippmann@mail.csuchico.edu>
2022-02-16 11:50:52 -0800

    first commit simple hello world and workflow

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// This program is a simple intro to C++ example of debugging
+int main()
+{
+    cout << "hello world CI gitHub actions!" << endl;
+  
+  return 0;
+}
